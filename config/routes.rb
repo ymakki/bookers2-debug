c27840bc53645f452aec43a77c5bad411f71a74b
ymakki <147698184+ymakki@users.noreply.github.com>
2023-12-24 07:11:17 +0000

    [Finish] 応用課題5

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
     get "following" => "relationships#following", as: "following"
   end
 
+  get "search", to: "searches#search"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
