8e5e44f345dbdc4dbe4008ff38f90c3216a783be
ymakki <147698184+ymakki@users.noreply.github.com>
2023-12-25 08:00:16 +0000

    [Add] 応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,18 +2,22 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
+    #インスタンス変数に変更
+    @comment = current_user.book_comments.new(book_comment_params)
     # ↑ 下記2行を省略
     #comment = BookComment.new(book_comment_params)
     #comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @comment.book_id = book.id
+    @comment.save
+    #redirect記述削除
+    #redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    #redirect記述削除
+    #redirect_back(fallback_location: root_path)
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,18 @@
 class FavoritesController < ApplicationController
 
+  before_action :authenticate_user!
+
   def create
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    # ↑　省略される元の文が分からない
-    favorite.save
-    redirect_back(fallback_location: root_path)
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
+    render 'replace_favorite'
   end
 
   def destroy
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_back(fallback_location: root_path)
+    @favorite = current_user.favorites.find_by(book_id: book.id)
+    @favorite.destroy
+    render 'replace_favorite'
   end
 end
