72ea99a6d2179cd508ec980f6a14d33233e77f85
ymakki <147698184+ymakki@users.noreply.github.com>
2023-12-26 04:24:02 +0000

    [Add] 応用課題6

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   devise_for :users
 
-  root to: 'homes#top'
+  root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
-
-  resources :users, only: [:index, :show, :update, :edit] do
+  resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-    get "followers" => "relationships#followers", as: "followers"
-    get "following" => "relationships#following", as: "following"
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
   end
-
-  get "search", to: "searches#search"
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/search', to: 'searches#search'
 end
