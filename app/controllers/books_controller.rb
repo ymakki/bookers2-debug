681150848daf22b1b68db0789689e0e41ee77dac
ymakki <147698184+ymakki@users.noreply.github.com>
2023-12-27 16:19:32 +0000

    [Finish] 応用課題8a & 9a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,10 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @book_comment = BookComment.new
+    @book_detail = Book.find(params[:id])
+    unless ReadCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.read_counts.create(book_id: @book_detail.id)
+    end
   end
 
   def index
