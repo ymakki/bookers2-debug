5a672df995179509503134d9e7d8d14878f63167
ymakki <147698184+ymakki@users.noreply.github.com>
2024-01-02 08:54:29 +0000

    [Finish]応用課題7d

--- app/controllers/event_notices_controller.rb
+++ app/controllers/event_notices_controller.rb
@@ -1,25 +1,30 @@
-class EventNoticesController < ApplicationController
+class Public::EventNoticesController < ApplicationController
+  before_action :authenticate_user!
 
   def new
+    # view/event_notices/new.html.erbのフォームでグループIDが必要な為
     @group = Group.find(params[:group_id])
   end
 
   def create
+    @group = Group.find(params[:group_id]) #newアクションと同じグループIDが入っている
+    @title = params[:title] #フォームで入力された内容が入っている
+    @body = params[:body] #フォームで入力された内容が入っている
 
-    @group = Group.find(params[:group_id])
-    @title = params[:title]
-    @body = params[:body]
-
+    # 上記でインスタンス変数のデータを変数eventのハッシュとして登録
     event = {
+      #キー　=> 　データ
       :group => @group,
       :title => @title,
-      :body => @body
-
+      :body => @body,
+      # enent[:group]のように呼び出すと@groupを呼び出せるようになる（event_mailerで使う）
     }
 
+    #app/mailers/event_mailer.rbのメソッドを使用している
     EventMailer.send_notifications_to_group(event)
-
+    # @group,@title,@bodyのデータを持ったまま、view/event_notices/sent.html.erbを表示する
     render :sent
+
   end
 
   def sent
@@ -27,4 +32,3 @@ class EventNoticesController < ApplicationController
   end
 
 end
-
--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,11 +1,17 @@
 class EventMailer < ApplicationMailer
 
+  # 下のクラスメソッド内で使用するメソッド
+  # このメソッド（アクション）が発動するとviews/event_mailer/send_notification.text.erbを呼出して、
+  # メールを引数のデータに従ってメールを作成する。
   def send_notification(member, event)
     @group = event[:group]
     @title = event[:title]
     @body = event[:body]
 
+    #メールを新規作成
+     #from→送り主、to→送り先、subject→メールタイトルを入力
     @mail = EventMailer.new()
+
     mail(
       from: ENV['MAIL_ADDRESS'],
       to:   member.email,
@@ -13,11 +19,14 @@ class EventMailer < ApplicationMailer
     )
   end
 
-  def self.send_notifications_to_group(event)
-    group = event[:group]
-    group.users.each do |member|
+  #　このクラスメソッドが呼出されるとメールをグループメンバにメールを送信する
+  # フォームでメールを送信を押したタイミングで発動(event_noticesコントローラの#create呼出時)
+  def self.send_notifications_to_group(event) #引数のeventはevent_noticesコントローラでハッシュでデータを入れている
+    group = event[:group] #group_idを呼出
+    group.users.each do |member| #group_idに紐づくuserを順番に取り出す
+      # 上のメソッドを発動すると各メンバー毎にメールが作られて、.deliver_nowで送信をしている
       EventMailer.send_notification(member, event).deliver_now
     end
   end
-
 end
+
